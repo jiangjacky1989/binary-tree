ad4cf70cf213e93e8518fbd0c701dfa5576c4728
EJJAAII <jianji.jiang@ericsson.com>
2021-02-03 15:56:06 +0800

    binary test

---
+++ binary_tree.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/3 15:53
+# @Author  : Jacky Jiang
+# @File    : binary_tree.py
+
+class TreeNode(object):
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
