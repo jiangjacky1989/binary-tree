4a8504f1640985ea5f41ffbe1fd9275774eec197
EJJAAII <jianji.jiang@ericsson.com>
2021-02-03 18:08:40 +0800

    binary test

--- binary_tree.py
+++ binary_tree.py
@@ -3,8 +3,28 @@
 # @Author  : Jacky Jiang
 # @File    : binary_tree.py
 
-class TreeNode(object):
-    def __init__(self,value):
-        self.value = value
-        self.left = None
-        self.right = None
+class Node(object):
+    """节点类"""
+    def __init__(self, x):
+        self.val = x
+        self.lchild = None
+        self.rchild = None
+
+root = Node(1)
+root.lchild = Node(2)
+root.rchild = Node(3)
+root.lchild.lchild = Node(4)
+root.lchild.rchild = Node(5)
+root.rchild.lchild = Node(6)
+root.rchild.rchild = Node(7)
+
+#前序遍历
+def pre_order(root):
+    if root is None:
+        return []
+    mid = [root.val]
+    left = pre_order(root.lchild)
+    right = pre_order(root.rchild)
+    return mid+left+right
+
+print(pre_order(root))
